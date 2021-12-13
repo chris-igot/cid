ca9238edcf1a099116886d10ad5ed918f5cbd5c7
cigot <c.bensi.igot@gmail.com>
2021-12-13 15:12:11 -0500

    Made all models inherit from a base model

--- cid_app/models/user.py
+++ cid_app/models/user.py
@@ -1,31 +1,23 @@
 from cid_app.config.mysqlconnection import connectToMySQL
-DB = "cid_db"
+from cid_app.models.model_base import ModelBase
 
-class User():
+class User(ModelBase):
+    table_name = "users"
     def __init__(self, data):
-        self.id = data["id"]
-        self.first_name = data["first_name"]
-        self.last_name = data["last_name"]
-        self.email = data["email"]
+        ModelBase.__init__(self,data)
+        self.id = data["id"] if "id" in data else None
+        self.first_name = data["first_name"] if "first_name" in data else None
+        self.last_name = data["last_name"] if "last_name" in data else None
+        self.email = data["email"] if "email" in data else None
+        self.password = data["password"] if "password" in data else None
     @classmethod
     def find_id(cls,id):
-        data = {
-            "id":id
-        }
-        query = "SELECT * FROM users WHERE id = %(id)s;"
-        results = connectToMySQL(DB).query_db(query,data)
-        if(results):
-            results[0].pop('password',None)
-            return results[0]
-        else:
-            return None
+        return cls.__find_id__(id)
     @classmethod
     def find_email(cls,form_data):
-        query = "SELECT * FROM users WHERE email = %(email)s;"
-        results = connectToMySQL(DB).query_db(query,form_data)
-        return results[0] if len(results) > 0 else None
-    
+        data = {"email":form_data["email"]}
+        return cls.__find_cols__(data)[0] if len(cls.__find_cols__(data)) > 0 else None
     @classmethod
     def save(cls,form_data):
-        query = "INSERT INTO users (first_name,last_name,email,password) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s);"
-        return connectToMySQL(DB).query_db(query,form_data)
+        return cls.__upsert__(form_data)
+        
